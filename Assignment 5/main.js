dcc26941701df03ef3e5027c47d0ac88c3043ead
UsmanShireen <usmanayan06@gmail.com>
2024-03-08 20:31:00 +0500

    first commit

---
+++ Assignment 5/main.js
@@ -0,0 +1,5 @@
+// stripping Names
+var firstname = "Usman\tShireen";
+var firstname1 = "Usman\nShireen";
+console.log(firstname);
+console.log(firstname1);
---
+++ Assignment 5/main.ts
@@ -0,0 +1,7 @@
+// stripping Names
+
+let firstname: string = "Usman\tShireen"
+let firstname1: string = "Usman\nShireen"
+
+console.log(firstname);
+console.log(firstname1);
